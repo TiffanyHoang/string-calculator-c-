af002d5ed9b799e645da8421c6bb854207574cda
Tiffany Hoang <tiffany.hoang@myob.com>
2020-09-21 17:07:31 +1000

    step 1-5

---
+++ StringCalculator_App/StringCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace StringCalculator_App
+{
+    public class StringCalculator
+    {
+        public static int Add(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            { 
+                return 0;
+            } else
+            {
+                string[] inputs = input.Split(new Char[] { ',', '\n'});
+               
+                int[] inputsInt = Array.ConvertAll(inputs, int.Parse);
+
+                return inputsInt.Sum();
+            }
+
+        }
+             
+    }
+}
---
+++ StringCalculator_Tests/StringCalculator_Test.cs
@@ -0,0 +1,53 @@
+using System;
+using Xunit;
+using StringCalculator_App;
+
+namespace StringCalculator_Tests
+{
+    public class StringCalculator_CalculatorShould
+    {
+        [Fact]
+        public void GivenAnEmptyString_Return0()
+        {
+            string input = "";
+
+            int expected = 0;
+            int actual = StringCalculator.Add(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GivenASingleNumberString_ReturnTheSingleNumber()
+        {
+            string input = "3";
+
+            int expected = 3;
+            int actual = StringCalculator.Add(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GivenTwoNumbersString_ReturnTheSumOfTheTwoNumbers()
+        {
+            string input = "3,3";
+
+            int expected = 6;
+            int actual = StringCalculator.Add(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GivenFourNumbersString_ReturnTheSumOfTheFourNumbers()
+        {
+            string input = "3,3,5,4";
+
+            int expected = 15;
+            int actual = StringCalculator.Add(input);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
